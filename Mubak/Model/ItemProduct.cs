4c9c6f49feaaab492b0943f141a84cd7420f9fc6
hugoviesi <viesihugo@hotmail.com>
2021-03-23 19:34:09 -0300

    Item Product Model

--- Mubak/Model/ItemProduct.cs
+++ Mubak/Model/ItemProduct.cs
@@ -8,6 +8,11 @@ namespace Model
 {
     class ItemProduct
     {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public decimal InitaryValue { get; set; }
+        public int Amount { get; set; }
+        public decimal TotalValue { get; set; }
 
     }
 }
