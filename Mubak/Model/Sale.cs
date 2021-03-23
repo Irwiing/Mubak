725c18aeba9e348463e20c64514f6c8bf1463038
saulogp <sauloproetti@gmail.com>
2021-03-23 20:04:36 -0300

    Sale Model

---
+++ Mubak/Model/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public string PaymentType { get; set; }
+        public DateTime DateSale { get; set; }
+        public string User { get; set; }
+        public string  Items { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
