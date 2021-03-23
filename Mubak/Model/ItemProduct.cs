51e6cca8162ad779eafebf77b680a4605234f536
hugoviesi <viesihugo@hotmail.com>
2021-03-23 20:45:46 -0300

    correção na gramática UnitaryValue

--- Mubak/Model/ItemProduct.cs
+++ Mubak/Model/ItemProduct.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Product { get; set; }
-        public decimal InitaryValue { get; set; }
+        public decimal UnitaryValue { get; set; }
         public int Amount { get; set; }
         public decimal TotalValue { get; set; }
 
