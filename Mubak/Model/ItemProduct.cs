555aa68eaecd306476c7ea5767650bb3ffbe65a3
hugoviesi <viesihugo@hotmail.com>
2021-03-23 19:58:23 -0300

    ItemProduct Dal

--- Mubak/Model/ItemProduct.cs
+++ Mubak/Model/ItemProduct.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model
+﻿namespace Model
 {
-    class ItemProduct
+    public class ItemProduct
     {
         public int Id { get; set; }
         public string Product { get; set; }
