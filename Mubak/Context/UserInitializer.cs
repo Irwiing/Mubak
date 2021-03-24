85d8d717a3d72dc22ff0ddba99929d5ccdf2d455
hugoviesi <viesihugo@hotmail.com>
2021-03-24 13:06:29 -0300

    user/phone/address completed

---
+++ Mubak/Context/UserInitializer.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Context
+{
+    class UserInitializer : DropCreateDatabaseIfModelChanges<UserContext>
+    {
+
+    }
+}
---
+++ Mubak/Mubak/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Model;
+using Context;
+
+namespace Mubak.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserContext _ctxUser = new UserContext();
+
+        // GET: Sale
+        public ActionResult Index()
+        {
+            return View(_ctxUser.Users.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctxUser.Users.Add(user);
+                _ctxUser.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_ctxUser.Users.First(u => u.Id == id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User userUpdate = _ctxUser.Users.First(u => u.Id == user.Id);
+                userUpdate.Situation = user.Situation;
+                userUpdate.Login = user.Login;
+                userUpdate.Password = user.Password;
+                userUpdate.CPF = user.CPF;
+                userUpdate.Name = user.Name;
+                userUpdate.Address = user.Address;
+                userUpdate.Phone = user.Phone;
+                userUpdate.Permission = user.Permission;
+                userUpdate.Email = user.Email;
+
+                _ctxUser.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        public ActionResult Details(int id)
+        {
+            return View(_ctxUser.Users.First(u => u.Id == id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(_ctxUser.Users.First(u => u.Id == id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id)
+        {
+            var user = _ctxUser.Users.First(u => u.Id == id);
+            _ctxUser.Users.Remove(user);
+            _ctxUser.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
