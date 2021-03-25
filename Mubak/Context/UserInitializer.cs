2a694ac48e5efb75ed932e8f960a3fec92ce5eaf
Irwiing <irwingtguarnieri@gmail.com>
2021-03-25 20:26:05 -0300

    ADICIONARO VARIOS LOGIN

--- Mubak/Context/UserInitializer.cs
+++ Mubak/Context/UserInitializer.cs
@@ -1,9 +1,22 @@
-﻿using System.Data.Entity;
+﻿using Model;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Context
 {
     class UserInitializer : DropCreateDatabaseIfModelChanges<UserContext>
     {
+        protected override void Seed(UserContext context)
+        {
+            var users = new List<User>
+            {
+                new User {Login = "admin", Password = "admin", Permission = "Administrator", Name = "Irwing", Situation = "ativo"},
+                new User {Login = "user", Password = "user", Permission = "Customer", Name = "Hugo", Situation = "ativo"}
+            };
+
+            users.ForEach(user => context.Users.Add(user));
+            context.SaveChanges();
+        }
 
     }
 }
